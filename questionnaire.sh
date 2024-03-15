590cfd57976c9ea088e62bf4f3502dcd2728975e
Juan Alzate <jupawe3092@gmail.com>
2024-03-15 13:26:32 +0000

    Save progress: 340.1

--- questionnaire.sh
+++ questionnaire.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "\n~~ Questionnaire ~~\n"
+echo -e "\n~~ Questionnaire ~~\n"
 
 QUESTION1="What's your name?"
 QUESTION2="Where are you from?"
+QUESTION3="What's your favorite coding website?"
 
 echo $QUESTION1 
 read NAME
 
 echo $QUESTION2
 read LOCATION
-
 
 echo Hello $NAME from $LOCATION.
